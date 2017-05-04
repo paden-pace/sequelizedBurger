7ed3a5e0c4b6c68fc7b51544470ce50f18d780ab
Paden Pace <pjpace27@yahoo.com>
2017-05-03 21:25:16 -0500

    basic layout of main files

---
+++ models/burger.js
@@ -0,0 +1,34 @@
+
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define("Burger", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      }
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false
+    } 
+  });
+  return Burger;
+}; 
+ 
+
+  // , 
+  //   {
+  //   classMethods: {
+  //     associate: function(models) {
+  //       // Using additional options like CASCADE etc for demonstration
+  //       // Can also simply do Task.belongsTo(models.User);
+  //       Post.belongsTo(models.Customer, {
+  //         foreignKey: {
+  //           allowNull: false
+  //         }
+  //       })
+  //     }
+  //   }
+  // }
